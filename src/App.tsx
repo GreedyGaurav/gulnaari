e03dfe512099ab58a51c4123f473bae8e06f4dfc
GreedyGaurav <gauravpinocchio786@gmail.com>
2025-04-21 23:52:26 +0530

    Updated something

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Categories from './components/Categories';
-import FeaturedProducts from './components/FeaturedProducts';
-import About from './components/About';
-import Testimonials from './components/Testimonials';
-import Newsletter from './components/Newsletter';
-import Footer from './components/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import FeaturedProducts from "./components/FeaturedProducts";
+import About from "./components/About";
+import Testimonials from "./components/Testimonials";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,11 +14,11 @@ function App() {
       <Navbar />
       <main>
         <Hero />
-        <Categories />
+        {/* <Categories />
         <FeaturedProducts />
         <About />
         <Testimonials />
-        <Newsletter />
+        <Newsletter /> */}
       </main>
       <Footer />
     </div>
